1660cc115da528377efa1f77f22371a7bcb06cc5
Sebastian Clavijo <sebas.cj@gmail.com>
2017-04-16 17:05:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+
+// app.use(express.static(__dirname));
+
+app.get('/', function(req,res){
+  res.render('index');
+});
+
+app.listen(PORT, function() {
+   console.log(`Api rest corriendo en http://localhost:${PORT}`);
+});
