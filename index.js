5576117fb0067e6e2215394a00b32992bb1eaeed
Sebastian Clavijo <sebas.cj@gmail.com>
2017-04-16 21:51:54 -0500

    add carousel and parallax

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path')
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
   res.render('index');
