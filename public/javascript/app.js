f536f51aba956e2138326bbd9efad0f8ff6553b2
Sebastian Clavijo <sebas.cj@gmail.com>
2017-04-18 00:00:11 -0500

    Add angular and create modals with angular ui

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $(".wrapper").show();
-  },200);
+var briefcaseApp = angular.module('briefcase', ['ui.bootstrap']);
+
+briefcaseApp.controller('MainCtrl', ['$uibModal', function($uibModal) {
+  var ctrl = this;
   var scroll_start = 0;
   var startchange = $('#startchange');
   var offset = startchange.offset();
   var windowSize = $(window).width();
 
-
+  setTimeout(function(){
+    $(".wrapper").show();
+  },200);
 
   function navOpacity() {
     scroll_start = $(this).scrollTop();
@@ -33,31 +35,43 @@ $(document).ready(function(){
 
   $( "#target" ).submit(function( event ) {
     $.ajax({
-    type: "POST",
-    url: '/sayHello',
-    data: {example: "example"},
-    success: function(data){
-      console.log(data);
-    },
+      type: "POST",
+      url: '/sayHello',
+      data: {example: "example"},
+      success: function(data){
+        console.log(data);
+      },
+    });
+    event.preventDefault();
   });
 
-  event.preventDefault();
-});
+  ctrl.apps = {
+    chocolates: "http://placehold.it/171x180",
+    monstruos: "http://placehold.it/171x180",
+    bicicletas: "http://placehold.it/171x180",
+    gatos: "http://placehold.it/171x180",
+    perros: "http://placehold.it/171x180",
+    casas: "http://placehold.it/171x180",
+    edificios: "http://placehold.it/171x180",
+    carros: "http://placehold.it/171x180"
+  }
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+  ctrl.openModal = function(name) {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'tpl/modal.html',
+      controllerAs: ctrl,
+      size: 'lg'
+    });
+    modalInstance.result.then(function (selectedItem) {
+      console.log('Modal Open');
+    }, function () {
+      console.log('Modal Close');
+    });
+  };
 
-$('.openModal').click(function(){
-  $('#myModal').modal('toggle');
-});
+}]);
 
-
-
-
-
-
-
-
-
-});
+// });
